9a7e54034e1f8bd59e996fd211f4cf92e6108276
Nate Chadwick <263952448+natechadwick-intsof@users.noreply.github.com>
2026-09-01 15:07:33 -0400

    fix(security): T2.6e commons-collections 3.2.2: disable unsafe deserialization at JVM startup (issue #176)
    commons-collections 3.2.2 is on the runtime classpath transitively. The 2
    cataloged CVEs (CVE-2015-6420, CVE-2015-7501) are CRITICAL deserialization
    gadget-chain CVEs from 2015 affecting InvokerTransformer, CloneTransformer,
    and InstantiateTransformer. The 3.x library added a kill switch:
    -Dorg.apache.commons.collections.enableUnsafeSerialization=false causes
    those classes to throw on use, neutralizing the gadget chain.
    
    This PR sets the kill switch at JVM startup in the 4 Tomcat start/stop
    scripts (TomcatStartup.sh, TomcatShutdown.sh, and their .bat siblings).
    The DTS service wrappers inherit JAVA_OPTS from the environment, so no
    changes are needed there.
    
    Refs #73 (parent epic), #176 (T2.6e workstream).
    Files: 4 changed, 4 insertions(+), 0 deletions(-).
    Full reactor build green (4:08, 60/60 modules).
    
    > Co-Authored by Mavis Mavis-Code using MiniMax-M3 with agent mavis.

--- deliverytiersuite/delivery-tier-suite/delivery-tier-distribution/src/main/rootFiles/TomcatShutdown.bat
+++ deliverytiersuite/delivery-tier-suite/delivery-tier-distribution/src/main/rootFiles/TomcatShutdown.bat
@@ -19,6 +19,6 @@ if NOT EXIST %JRE_HOME% (
 SET JRE_HOME=%SCRIPT_DIR%\..\JRE
 )
 
-set JAVA_OPTS=%JAVA_OPTS% -Dhttps.protocols=TLSv1.2 -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -Dfile.encoding=UTF-8 -Xmx1024m -Dnet.sf.ehcache.skipUpdateCheck=true -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:NewSize=256m -XX:SurvivorRatio=16 -Djava.endorsed.dirs=%SERVER_DIR%\endorsed -Dcatalina.base=%SERVER_DIR% -Dcatalina.home=%SERVER_DIR% -Djava.io.tmpdir=%SERVER_DIR%\temp -Dderby.system.home=%SERVER_DIR%\derbydata
+set JAVA_OPTS=%JAVA_OPTS% -Dhttps.protocols=TLSv1.2 -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -Dfile.encoding=UTF-8 -Xmx1024m -Dnet.sf.ehcache.skipUpdateCheck=true -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:NewSize=256m -XX:SurvivorRatio=16 -Djava.endorsed.dirs=%SERVER_DIR%\endorsed -Dcatalina.base=%SERVER_DIR% -Dcatalina.home=%SERVER_DIR% -Djava.io.tmpdir=%SERVER_DIR%\temp -Dderby.system.home=%SERVER_DIR%\derbydata -Dorg.apache.commons.collections.enableUnsafeSerialization=false
 set CATALINA_HOME=%SERVER_DIR%
 "%SERVER_DIR%\bin\catalina.bat" stop
--- deliverytiersuite/delivery-tier-suite/delivery-tier-distribution/src/main/rootFiles/TomcatStartup.sh
+++ deliverytiersuite/delivery-tier-suite/delivery-tier-distribution/src/main/rootFiles/TomcatStartup.sh
@@ -19,6 +19,6 @@ export JRE_HOME=`pwd`
 
 cd bin
 
-export JAVA_OPTS="$JAVA_OPTS -Dhttps.protocols=TLSv1.2 -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -Dfile.encoding=UTF-8 -Xmx1024m -Dnet.sf.ehcache.skipUpdateCheck=true -Djava.endorsed.dirs=$SERVER_DIR/endorsed  -Dcatalina.base=$SERVER_DIR -Dcatalina.home=$SERVER_DIR -Djava.io.tmpdir=$SERVER_DIR/temp -Dderby.system.home=$SERVER_DIR/derbydata"
+export JAVA_OPTS="$JAVA_OPTS -Dhttps.protocols=TLSv1.2 -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -Dfile.encoding=UTF-8 -Xmx1024m -Dnet.sf.ehcache.skipUpdateCheck=true -Djava.endorsed.dirs=$SERVER_DIR/endorsed  -Dcatalina.base=$SERVER_DIR -Dcatalina.home=$SERVER_DIR -Djava.io.tmpdir=$SERVER_DIR/temp -Dderby.system.home=$SERVER_DIR/derbydata -Dorg.apache.commons.collections.enableUnsafeSerialization=false"
 export CATALINA_HOME=$SERVER_DIR
 $SERVER_DIR/bin/catalina.sh run
